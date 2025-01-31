456148c771264af6c809831390041def674d169c
julian castro <jucastrohenao@gmail.com>
2025-01-30 20:12:00 -0500

    progress in login

--- src/Modales/Login.jsx
+++ src/Modales/Login.jsx
@@ -1,7 +1,35 @@
-
+import { useState } from "react"
+import "./StyleLogin.css"
 
 
 export default function Login({closeModal}){
+
+    const [information, setInformation]=useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const handleChange=(e)=>{
+        const {name,value}=e.target
+        setInformation({
+            ...information,
+            [name]:value
+        })
+    }
+
+    const handlesubmit=()=>{
+        if(information.email== ""  || information.password==""){
+            return alert("rellene los datos")
+        }
+        if(information.email.trim()== " "  || information.password.trim() ==" "){
+            return "No pueden haber espacios"
+        }
+        alert(`tus datos son ${information.email} y ${information.name}`)
+    }
+
+ 
+
     return(
         <div className="container_register">
 
@@ -11,15 +39,26 @@ export default function Login({closeModal}){
                 <button className="close_register" onClick={closeModal}>X</button> 
                 </div>
 
-            <div className="form"> 
-            <input className="style_input" type="email" placeholder="email" />
-            <input className="style_input" type= "password" placeholder="password" />
+            <div className="login">
+
+            <div className="img_login">
+            <img className="img_log" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYV7teAdHYe9eHib7ZW-VA_3XiGIA3B3OqwQ&s" alt="imagen" />
             </div>
 
-            <div className="btn">   
-                <button className="btn_register" >Login</button>
-                </div>
+            <div className="input_form">
+                <h1>Inicio Sesion</h1>
+                <input className="login_inp" name= "email" type="email"   value= {information.email}   placeholder="email"  onChange={handleChange}/>
+                <input className="login_inp" type= "password"  name= "password"  value={information.password} placeholder="password"  onChange={handleChange}/>
 
+                <div className="btn">   
+                <button className="btn_register" onClick={()=> handlesubmit()} >Login</button>
+            </div>
+
+        </div>
+            
+    </div>
+
+           
             </div>
 
         </div>
